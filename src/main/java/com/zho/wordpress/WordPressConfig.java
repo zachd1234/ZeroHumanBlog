e9389afce483bdfa17fca7e210c769f5573de449
Zach Derhake <zachderhake@Zachs-Laptop.router92c184.com>
2025-01-03 23:30:11 -0800

    refactor complete

--- src/main/java/com/zho/wordpress/WordPressConfig.java
+++
@@ -1,7 +0,0 @@
-package com.zho.wordpress;
-
-public class WordPressConfig {
-    public static final String BASE_URL = "https://mbt.dsc.mybluehost.me/wp-json/wp/v2/";
-    public static final String USERNAME = "zach";
-    public static final String APPLICATION_PASSWORD = "anI6 BOd7 RDLL z4ET 7z0U fTrt";
-} 
